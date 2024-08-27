6f1b8d77f233645e789c1682f9257afaba6493c4
Abhi Yerra <abhi@opszero.com>
2024-08-27 10:07:14 -0700

    Add output

--- outputs.tf
+++ outputs.tf
@@ -4,32 +4,32 @@
 
 output "cluster_id" {
   description = "Cluster name"
-  value       = try(aws_memorydb_cluster.this[0].id, "")
+  value       = aws_memorydb_cluster.this.id
 }
 
 output "cluster_arn" {
   description = "The ARN of the cluster"
-  value       = try(aws_memorydb_cluster.this[0].arn, "")
+  value       = aws_memorydb_cluster.this.arn
 }
 
 output "cluster_endpoint_address" {
   description = "DNS hostname of the cluster configuration endpoint"
-  value       = try(aws_memorydb_cluster.this[0].cluster_endpoint[0].address, "")
+  value       = aws_memorydb_cluster.this.cluster_endpoint[0].address
 }
 
 output "cluster_endpoint_port" {
   description = "Port number that the cluster configuration endpoint is listening on"
-  value       = try(aws_memorydb_cluster.this[0].cluster_endpoint[0].port, "")
+  value       = aws_memorydb_cluster.this.cluster_endpoint[0].port
 }
 
 output "cluster_engine_patch_version" {
   description = "Patch version number of the Redis engine used by the cluster"
-  value       = try(aws_memorydb_cluster.this[0].engine_patch_version, "")
+  value       = aws_memorydb_cluster.this.engine_patch_version
 }
 
 output "cluster_shards" {
   description = "Set of shards in this cluster"
-  value       = try(aws_memorydb_cluster.this[0].shards, [])
+  value       = aws_memorydb_cluster.this.shards
 }
 
 ################################################################################
@@ -48,17 +48,17 @@ output "users" {
 
 output "acl_id" {
   description = "Name of the ACL"
-  value       = try(aws_memorydb_acl.this[0].id, "")
+  value       = aws_memorydb_acl.this.id
 }
 
 output "acl_arn" {
   description = "The ARN of the ACL"
-  value       = try(aws_memorydb_acl.this[0].arn, "")
+  value       = aws_memorydb_acl.this.arn
 }
 
 output "acl_minimum_engine_version" {
   description = "The minimum engine version supported by the ACL"
-  value       = try(aws_memorydb_acl.this[0].minimum_engine_version, "")
+  value       = aws_memorydb_acl.this.minimum_engine_version
 }
 
 ################################################################################
@@ -67,12 +67,12 @@ output "acl_minimum_engine_version" {
 
 output "parameter_group_id" {
   description = "Name of the parameter group"
-  value       = try(aws_memorydb_parameter_group.this[0].id, "")
+  value       = aws_memorydb_parameter_group.this.id
 }
 
 output "parameter_group_arn" {
   description = "The ARN of the parameter group"
-  value       = try(aws_memorydb_parameter_group.this[0].arn, "")
+  value       = aws_memorydb_parameter_group.this.arn
 }
 
 ################################################################################
@@ -81,15 +81,15 @@ output "parameter_group_arn" {
 
 output "subnet_group_id" {
   description = "Name of the subnet group"
-  value       = try(aws_memorydb_subnet_group.this[0].id, "")
+  value       = aws_memorydb_subnet_group.this.id
 }
 
 output "subnet_group_arn" {
   description = "ARN of the subnet group"
-  value       = try(aws_memorydb_subnet_group.this[0].arn, "")
+  value       = aws_memorydb_subnet_group.this.arn
 }
 
 output "subnet_group_vpc_id" {
   description = "The VPC in which the subnet group exists"
-  value       = try(aws_memorydb_subnet_group.this[0].vpc_id, "")
+  value       = aws_memorydb_subnet_group.this.vpc_id
 }
